9d36eafd8228b00624cc5ba7f101ddc75af3de2b
wusongbo <wusongbo@gomeplus.com>
2017-10-13 19:06:28 +0800

    修改packjson，前端目录结构路由和整理。

---
+++ main.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const path = require('path')
+const ejs = require('ejs')
+const app = express()
+const domain = 'https://work.gomeplus.com/'
+
+app.engine('html', ejs.renderFile)
+app.set('views', path.join(__dirname, './views'))
+app.set('view engine', 'html')
+app.use('/css', express.static('views/css'))
+app.use('/js', express.static('views/js'))
+const lobj = {
+	windows: 'windows',
+	mac: '苹果Mac',
+	and: '安卓',
+	ios: '苹果'
+}
+/* 
+开始获取数据并，路由
+*/
+// 首页
+app.get("/", function(req, res) {
+	let result = {}
+    res.render("index", {title : "首页", domain : domain})
+})
+// 下载
+app.get("/downloads", function(req, res) {
+    res.render("page/main", {title : "下载", domain : domain})
+})
+// 关于我们
+app.get("/aboutus", function(req, res) {
+    res.render("page/about", {title : "关于我们", domain : domain, para: '0'})
+})
+// 联系我们
+app.get("/contactus", function(req, res) {
+    res.render("page/about", {title : "联系我们", domain : domain, para: '1'})
+})
+// 常见问题
+app.get("/faq", function(req, res) {
+    res.render("index", {title : "常见问题", domain : domain})
+})
+// 服务协议
+app.get("/terms", function(req, res) {
+    res.render("index", {title : "服务协议", domain : domain})
+})
+// 隐私政策
+app.get("/privacy", function(req, res) {
+    res.render("page/version", {title : "隐私政策", domain : domain})
+})
+// 日志
+app.get("/updates/(/.*$/)?-v(/.*)?", function(req, res) {
+    res.render("index", {title : "", domain : domain})
+})
+var server = app.listen(3000, function () {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+})
+
